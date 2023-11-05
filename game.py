8fbedc55c0cddf4e9e9f1229863f78946b1f6ca8
Misha <fshf@dgs.df>
2023-11-05 14:42:47 +0200

    finised game

--- game.py
+++ game.py
@@ -8,6 +8,15 @@ SIZE = (WIDHT,HEIGHT)
 
 window = pygame.display.set_mode(SIZE)
 background_color = (60,159,63)
+background_image = pygame.transform.scale(pygame.image.load("background.png"), SIZE)
+
+score = 0
+
+pygame.font.init()
+font2 = pygame.font.Font(None, 40)
+
+
+
 window.fill(background_color)
 clock = pygame.time.Clock()
 
@@ -33,16 +42,24 @@ class Tube(GameSprite):
     def update_up(self):
         self.rect.x -= self.speed
         if self.rect.x <= 0:
-            self.rect.x = WIDHT
+            self.rect.x = WIDHT + randint(0,100)
             self.rect = pygame.Rect(self.rect.x,self.rect.y, self.rect.width, randint(50,HEIGHT/2-50))
             self.image = pygame.transform.scale(self.original, (self.rect.width,self.rect.height))
-    
+            global score
+            score += 1
+            self.speed = randint(3,10)
+
+
     def update_down(self):
         self.rect.x -= self.speed
         if self.rect.x <= 0:
-            self.rect.x = WIDHT
+            self.rect.x = WIDHT + randint(0,100)
             self.rect = pygame.Rect(self.rect.x, randint(HEIGHT/2, HEIGHT-100), self.rect.width, self.rect.height)
             self.image = pygame.transform.scale(self.original, (self.rect.width,self.rect.height))
+            global score
+            score += 1
+            self.speed = randint(3,10)
+
 
 
 
@@ -58,7 +75,8 @@ while game:
             game = False
 
     if not finish:
-        window.fill(background_color)
+        window.blit(background_image, (0,0))
+       
         bird.update()
         bird.reset()
         up_tube.update_up()
@@ -73,6 +91,8 @@ while game:
             text = font1.render("Ти програв", True, (255,0,0))
             window.blit(text, (WIDHT/2-100, HEIGHT/2))
 
+        score_text = font2.render(str(score), True, (255,255,255))
+        window.blit(score_text, (0,0))
 
 
     pygame.display.update()
